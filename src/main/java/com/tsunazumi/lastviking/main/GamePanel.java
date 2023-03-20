d06d81139eac70227babc2f5f4559abab1b2497b
Tom Kealamakia <tkealamakia@gmail.com>
2023-03-19 22:19:10 -0600

    first commit

---
+++ src/main/java/com/tsunazumi/lastviking/main/GamePanel.java
@@ -0,0 +1,78 @@
+package com.tsunazumi.lastviking.main;
+
+import com.tsunazumi.lastviking.inputs.KeyboardInputs;
+import com.tsunazumi.lastviking.inputs.MouseInputs;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class GamePanel extends JPanel {
+
+  private MouseInputs mouseInputs;
+  private float xDelta = 100, yDelta = 100;
+  private float xDir = 0.016f, yDir = 0.016f;
+  private int frames = 0;
+  private long lastCheck = 0;
+  private Color color = new Color(0,200,0);
+  private Random random;
+
+  public GamePanel() {
+    random = new Random();
+    mouseInputs = new MouseInputs(this);
+    addKeyListener(new KeyboardInputs(this));
+    addMouseListener(mouseInputs);
+    addMouseMotionListener(mouseInputs);
+  }
+
+  public void changeXDelta(int value) {
+    this.xDelta += value;
+  }
+
+  public void changeYDelta(int value) {
+    this.yDelta += value;
+  }
+
+  public void setRectPos(int x, int y) {
+    this.xDelta = x;
+    this.yDelta = y;
+  }
+
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+
+    updateRectangle();
+    g.setColor(color);
+    g.fillRect((int)xDelta, (int)yDelta, 200, 50);
+
+    frames++;
+    if (System.currentTimeMillis() - lastCheck > 1000) {
+      lastCheck = System.currentTimeMillis();
+      System.out.println("FPS: " + frames);
+      frames = 0;
+    }
+
+    repaint();
+  }
+
+  private void updateRectangle() {
+    xDelta += xDir;
+    if (xDelta > 400 || xDelta < 0) {
+      xDir *= -1;
+      color = getRandomColor();
+    }
+    yDelta += yDir;
+    if (yDelta > 400 || yDelta < 0) {
+      yDir *= -1;
+      color = getRandomColor();
+    }
+
+  }
+
+  private Color getRandomColor() {
+    int r = random.nextInt(255);
+    int b = random.nextInt(255);
+    int g = random.nextInt(255);
+    return new Color(r,g,b);
+  }
+}
---
+++ src/main/java/com/tsunazumi/lastviking/main/GameWindow.java
@@ -0,0 +1,16 @@
+package com.tsunazumi.lastviking.main;
+
+import javax.swing.*;
+
+public class GameWindow {
+  private JFrame jframe;
+
+  public GameWindow(GamePanel gamePanel) {
+    jframe = new JFrame();
+    jframe.setSize(400, 400);
+    jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    jframe.add(gamePanel);
+    jframe.setLocationRelativeTo(null);
+    jframe.setVisible(true);
+  }
+}
